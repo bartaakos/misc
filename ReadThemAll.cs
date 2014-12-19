7b945788dd1f7f362203f3ed9aa6c900391080c1
Barta Ákos <brta.akos@gmail.com>
2014-12-19 11:29:36 +0100

    Create ReadThemAll.cs

---
+++ ReadThemAll.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace readThemAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetIn(new StreamReader(Console.OpenStandardInput(8192)));
+            while (Console.In.Peek() != -1)
+            {
+                string input = Console.In.ReadLine();
+                Console.WriteLine("Data read was " + input);
+            }
+        }
+    }
+}
